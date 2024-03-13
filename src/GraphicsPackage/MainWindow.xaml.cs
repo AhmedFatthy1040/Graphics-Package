34397c5550743e8fade3a3f1ebc5fbbc134951a4
justFadel19 <ahmedfadelaf885794@gmail.com>
2024-03-13 23:56:29 +0200

    Gui MainWindow modified to Dark mode

--- src/GraphicsPackage/MainWindow.xaml.cs
+++ src/GraphicsPackage/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace GraphicsPackage;
 public partial class MainWindow : Window
 {
     private string x1 = "";
+    private string y1 = "";
     public MainWindow()
     {
         InitializeComponent();
@@ -29,6 +30,16 @@ public partial class MainWindow : Window
 
     private void DrawLineDDA_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("Text entered: " + x1);
+        MessageBox.Show("The text entered is: " + x1);
+    }
+
+    private void Y1_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        y1 = Y1.Text;
+    }
+
+    private void DrawLine_Bresenham_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("The text entered is: " + y1);
     }
 }
